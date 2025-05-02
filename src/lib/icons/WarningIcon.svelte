c979b175b61af78a1141a69feec0fefeb0b06a86
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-02 17:46:34 -0300

    svg icons: error, info, warning

---
+++ src/lib/icons/WarningIcon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { SVGAttributes } from "svelte/elements"
+
+	interface Props extends SVGAttributes<SVGSVGElement> {}
+
+	let { ...rest }: Props = $props()
+</script>
+
+<svg
+	{...rest}
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 -960 960 960"
+	height={rest.width ?? "24px"}
+	width={rest.width ?? "24px"}
+	fill="var(--sc-color-text)"
+>
+	<path
+		d="m40-120 440-760 440 760H40Zm138-80h604L480-720 178-200Zm302-40q17 0 28.5-11.5T520-280q0-17-11.5-28.5T480-320q-17 0-28.5 11.5T440-280q0 17 11.5 28.5T480-240Zm-40-120h80v-200h-80v200Zm40-100Z"
+	/>
+</svg>
