7f8e13c2f3626be846d6a494c75403bacbc003f3
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-02 10:05:17 -0300

    select component

---
+++ src/lib/components/select/Select.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { HTMLSelectAttributes } from "svelte/elements"
+	import classMapUtil from "../../utils/classMapUtil.js"
+	import styles from "./Select.module.css"
+
+	interface Props extends HTMLSelectAttributes {}
+
+	let { class: className = "", children, ...rest }: Props = $props()
+</script>
+
+<div
+	class={classMapUtil({
+		[className as string]: true,
+		[styles.select]: true
+	})}
+>
+	<select {...rest}>
+		{@render children?.()}
+	</select>
+</div>
