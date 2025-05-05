5bc59eaf2c5477d6ac0b61df0a9f72a874282c65
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-05 16:11:03 -0300

    scroll navigator childs

--- src/lib/actions/scrollNavigatorAction.ts
+++ src/lib/actions/scrollNavigatorAction.ts
@@ -6,6 +6,10 @@ export default function scrollNavigatorAction(
 	node: HTMLElement,
 	props: Props = {}
 ) {
+	function getChilds() {
+		return Array.from(node.children) as HTMLElement[]
+	}
+
 	function update() {
 		const isScrollable = node.scrollWidth > node.clientWidth
 		const isFirst = node.scrollLeft === 0
@@ -14,37 +18,37 @@ export default function scrollNavigatorAction(
 	}
 
 	function getNextElement() {
-		const children = Array.from(node.children) as HTMLElement[]
+		const childs = getChilds()
 		const scrollLeft = node.scrollLeft
 		const scrollWidth = node.clientWidth / 3
 
-		if (scrollLeft === 0 && children.length > 1) {
-			return children[1]
+		if (scrollLeft === 0 && childs.length > 1) {
+			return childs[1]
 		}
 
-		for (const child of children) {
+		for (const child of childs) {
 			if (child.offsetLeft > scrollLeft + scrollWidth) {
 				return child
 			}
 		}
-		return children[children.length - 1]
+		return childs[childs.length - 1]
 	}
 
 	function getPrevElement() {
-		const children = Array.from(node.children) as HTMLElement[]
+		const childs = getChilds()
 		const scrollLeft = node.scrollLeft
 		const scrollWidth = node.clientWidth / 3
 
-		if (scrollLeft < scrollWidth && children.length > 0) {
-			return children[0]
+		if (scrollLeft < scrollWidth && childs.length > 0) {
+			return childs[0]
 		}
 
-		for (let i = children.length - 1; i >= 0; i--) {
-			if (children[i].offsetLeft < scrollLeft - scrollWidth / 2) {
-				return children[i]
+		for (let i = childs.length - 1; i >= 0; i--) {
+			if (childs[i].offsetLeft < scrollLeft - scrollWidth / 2) {
+				return childs[i]
 			}
 		}
-		return children[0]
+		return childs[0]
 	}
 
 	function scrollToElement(element: HTMLElement) {
@@ -63,9 +67,9 @@ export default function scrollNavigatorAction(
 		next: () => scrollToElement(getNextElement()),
 		prev: () => scrollToElement(getPrevElement()),
 		goTo: (index: number) => {
-			const children = Array.from(node.children) as HTMLElement[]
-			const clampedIndex = Math.max(0, Math.min(index, children.length - 1))
-			scrollToElement(children[clampedIndex])
+			const childs = getChilds()
+			const clampedIndex = Math.max(0, Math.min(index, childs.length - 1))
+			scrollToElement(childs[clampedIndex])
 		}
 	}
 
