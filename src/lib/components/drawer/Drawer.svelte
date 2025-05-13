c3eec9616cc7764b67d45491583770a8546645fb
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-13 12:01:23 -0300

    switch style, drawer props

--- src/lib/components/drawer/Drawer.svelte
+++ src/lib/components/drawer/Drawer.svelte
@@ -3,12 +3,11 @@
 	import type { HTMLAttributes } from "svelte/elements"
 	import classMapUtil from "../../utils/classMapUtil.js"
 	import styles from "./Drawer.module.css"
-	import { clickOutsideAction, resizeAction } from "../../actions/index.js"
+	import { clickOutsideAction } from "../../actions/index.js"
 	import type { PositionTypeNoCenter } from "../../types/index.js"
 
 	export interface Props extends HTMLAttributes<HTMLDivElement> {
 		isOpen?: boolean
-		variant?: "permanent" | "temporary"
 		position?: PositionTypeNoCenter
 		handleClose?: () => void
 		header?: Snippet<[]>
@@ -18,34 +17,24 @@
 	let {
 		class: className = "",
 		isOpen = false,
-		variant = "temporary",
 		position = "left",
 		handleClose,
 		header,
 		content,
 		...rest
 	}: Props = $props()
-
-	const isAutoClose = variant === "temporary" && handleClose
 </script>
 
 <div
 	class={classMapUtil({
 		[className as string]: true,
 		[styles.drawer]: true,
-		[styles.show]: isOpen || variant === "permanent",
+		[styles.show]: isOpen,
 		[styles[position]]: true
 	})}
 	use:clickOutsideAction={{
 		handler: () => {
-			if (isAutoClose) handleClose()
-		}
-	}}
-	use:resizeAction={{
-		handler: () => {
-			if (isAutoClose && window.innerWidth > 768) {
-				handleClose()
-			}
+			handleClose?.()
 		}
 	}}
 	{...rest}
