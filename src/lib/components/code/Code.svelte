78420c03401d472d74a012a59a8317106a985271
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-15 09:35:36 -0300

    code component

---
+++ src/lib/components/code/Code.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { HTMLAttributes } from "svelte/elements"
+	import classMapUtil from "../../utils/classMapUtil.js"
+	import Window from "../window/index.js"
+	import styles from "./Code.module.css"
+
+	interface Props extends HTMLAttributes<HTMLDivElement> {}
+
+	let { class: className = "", children, ...rest }: Props = $props()
+</script>
+
+<Window
+	class={classMapUtil(className, [styles, className], styles.code)}
+	{...rest}
+>
+	{@render children?.()}
+</Window>
