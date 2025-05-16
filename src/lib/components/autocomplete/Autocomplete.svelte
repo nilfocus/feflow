f39f904f246e4a65ea58d130bf12f0fd7024c0f1
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-16 17:37:31 -0300

    autocomplete component style and actions

--- src/lib/components/autocomplete/Autocomplete.svelte
+++ src/lib/components/autocomplete/Autocomplete.svelte
@@ -6,21 +6,41 @@
 	import { classMapUtil } from "../../utils/index.js"
 
 	let focusedItem = $state(-1)
+	let inputValue = $state("")
 
-	let filtered = [
+	const data = [
 		{ id: 1, title: "Item 1" },
 		{ id: 2, title: "Item 2" },
 		{ id: 3, title: "Item 3" },
 		{ id: 4, title: "Item 4" }
 	]
 
+	let filtered: typeof data = $state([])
+
 	function handleSelect(item: string) {
 		console.log(item)
+		focusedItem = -1
+		filtered = []
+		inputValue = ""
 	}
 
 	function handleFocusChange(focused: number) {
-		console.log(focused)
 		focusedItem = focused
+	}
+
+	function handleOnInput(
+		e: Event & {
+			currentTarget: EventTarget & HTMLInputElement
+		}
+	) {
+		const value = e.currentTarget.value.toLowerCase()
+		inputValue = value
+
+		const newData = data.filter((v) => {
+			if (value.length === 0) return
+			return v.title.toLowerCase().includes(value)
+		})
+		filtered = newData
 	}
 </script>
 
@@ -30,12 +50,14 @@
 			[
 				keyboardNavigationAction as ActionType<HTMLElement>,
 				{
-					data: filtered,
+					data: $state.snapshot(filtered),
 					onSelect: handleSelect,
 					onFocusChange: handleFocusChange
 				}
 			]
 		]}
+		oninput={handleOnInput}
+		value={inputValue}
 	/>
 
 	<div class={styles.items}>
