08ffa6ea78a1f48f1d2ea1318bb3c71a1abd62a4
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-19 17:53:36 -0300

    accordion button to input

--- src/lib/components/accordion/Accordion.svelte
+++ src/lib/components/accordion/Accordion.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-	import type { HTMLButtonAttributes } from "svelte/elements"
+	import type { HTMLInputAttributes } from "svelte/elements"
 	import type { VariantType } from "../../types/index.js"
-	import classMapUtil from "../../utils/classMapUtil.js"
-	import styles from "./Accordion.module.css"
-	import Button from "../button/index.js"
+	import { KeyboardArrowLeftIcon } from "../../icons/index.js"
 
-	interface Props extends HTMLButtonAttributes {
-		isOpen: boolean
+	interface Props extends HTMLInputAttributes {
 		label: string
 		variant?: VariantType
 	}
 
 	let {
 		class: className = "",
-		isOpen,
 		label,
 		variant = "contained",
 		children,
@@ -21,22 +17,63 @@
 	}: Props = $props()
 </script>
 
-<div class={styles.accordion}>
-	<Button
-		class={classMapUtil({
-			[styles.active]: isOpen
-		})}
-		{variant}
-		{...rest}
-	>
-		{label}
-	</Button>
-	<div
-		class={classMapUtil({
-			[styles.content]: true,
-			[styles.active]: isOpen
-		})}
-	>
+<div class="accordion">
+	<input type="radio" id={rest.id} name="accordion" {...rest} />
+	<label for={rest.id} class="header">
+		<label>{label}</label>
+		<div class="icon">
+			<KeyboardArrowLeftIcon />
+		</div>
+	</label>
+	<div class="content">
 		{@render children?.()}
 	</div>
 </div>
+
+<style>
+	.accordion {
+		margin-bottom: 5px;
+	}
+
+	.accordion input[type="radio"] {
+		display: none;
+	}
+
+	.accordion .content {
+		height: 0px;
+		padding: 0px;
+		background: var(--feflow-color-surface);
+		border-top: 1px solid var(--feflow-color-border);
+		overflow: hidden;
+		transition: all 0.3s ease;
+		opacity: 0;
+	}
+
+	.accordion input[type="radio"]:checked + .header + .content {
+		height: auto;
+		padding: 15px;
+		opacity: 1;
+	}
+
+	.icon {
+		display: flex;
+		align-items: center;
+		transition: all 0.5s ease;
+		transform: rotate(-90deg);
+	}
+
+	.accordion input[type="radio"]:checked + .header .icon {
+		transform: rotate(90deg);
+	}
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 8px;
+		background: var(--feflow-color-surface);
+		cursor: pointer;
+		font-weight: bold;
+		text-align: left;
+	}
+</style>
--- src/routes/(examples)/accordion/+page.svelte
+++ src/routes/(examples)/accordion/+page.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
 	import { Accordion } from "@/lib/index.js"
-
-	let isOpen = $state(false)
 </script>
 
-<Accordion
-	onclick={() => {
-		isOpen = !isOpen
-	}}
-	variant="text"
-	label="test"
-	{isOpen}
->
-	<span>test</span>
-</Accordion>
-
-<Accordion label="test" isOpen={true}>
-	<span>test</span>
-</Accordion>
+{#each Array.from(Array(10)) as _, i}
+	<Accordion id="sec{i}" variant="text" label="test {i}">
+		<span>test</span>
+	</Accordion>
+{/each}
