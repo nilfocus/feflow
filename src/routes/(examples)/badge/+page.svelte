b64f01c16aa9ae9563fe4f9f5307dec53f196621
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-20 16:38:50 -0300

    badge more examples

--- src/routes/(examples)/badge/+page.svelte
+++ src/routes/(examples)/badge/+page.svelte
@@ -12,16 +12,44 @@
 	}
 </script>
 
-<Badge
-	class="rounded-full bg-warning text-on-warning"
-	size="lg"
-	variant="contained"
->
-	{count}
-</Badge>
-<br />
-<br />
-<Button onclick={decrement}>decrement (-)</Button>
-<br />
-<br />
-<Button variant="contained" onclick={increment}>increment (+)</Button>
+<div style="width: 500px; margin: 5rem auto;">
+	<div style="padding: 1rem 0;">
+		<Badge
+			class="rounded-full bg-success text-on-success"
+			size="lg"
+			variant="contained"
+		>
+			success
+		</Badge>
+
+		<Badge
+			class="rounded-full bg-error text-on-error"
+			size="lg"
+			variant="contained"
+		>
+			error
+		</Badge>
+
+		<Badge
+			class="rounded-full bg-warning text-on-warning"
+			size="lg"
+			variant="contained"
+		>
+			warning
+		</Badge>
+	</div>
+
+	<Badge
+		class="rounded-full bg-warning text-on-warning"
+		size="lg"
+		variant="contained"
+	>
+		{count}
+	</Badge>
+	<br />
+	<br />
+	<Button disabled={count === 0} onclick={decrement}>decrement (-)</Button>
+	<br />
+	<br />
+	<Button variant="contained" onclick={increment}>increment (+)</Button>
+</div>
