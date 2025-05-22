2150287d64a271e8e82dfbe2491d68e3b109b614
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-22 08:51:46 -0300

    navbar styles and examples

--- src/routes/(examples)/navbar/+page.svelte
+++ src/routes/(examples)/navbar/+page.svelte
@@ -1,103 +1,124 @@
 <script lang="ts">
 	import { InputSearch, Menu, Navbar } from "@/lib/index.js"
+	import type { Snippet } from "svelte"
 </script>
 
-<Navbar id="nav1" variant="full" align="left">
-	{#snippet left()}
-		<a href="/">
-			<Navbar.Item aria-current="page">active</Navbar.Item>
-		</a>
-		<a href="/">
-			<Navbar.Item>test</Navbar.Item>
-		</a>
-		<Menu id="one" class="border-0">
-			{#snippet anchor()}
-				<Navbar.Item>Open menu</Navbar.Item>
-			{/snippet}
+{#snippet logo()}
+	<h2>Lo<span style="color: aqua;">Go</span></h2>
+{/snippet}
 
-			{#snippet items(handleClose)}
-				<Menu.Item onclick={handleClose}>test1</Menu.Item>
-				<a href="/">
-					<Menu.Item>test2</Menu.Item>
-				</a>
-				<a href="/">
-					<Menu.Item>test3</Menu.Item>
-				</a>
-			{/snippet}
-		</Menu>
-		<Navbar.Item variant="contained">test4</Navbar.Item>
-		<Navbar.Item variant="outlined">test5</Navbar.Item>
-	{/snippet}
+{#snippet logoWithToggler(toggler: Snippet)}
+	<div
+		style="
+		display: flex;
+		align-items: center; 
+		justify-content: space-between;
+		padding: 0 1rem;
+		"
+	>
+		{@render toggler?.()}
+		{@render logo?.()}
+	</div>
+{/snippet}
 
-	{#snippet center()}
-		<InputSearch variant="contained" placeholder="test..." />
-	{/snippet}
+{#snippet container(children: Snippet)}
+	<div
+		style="
+		width: 500px; 
+		max-height: 350px; 
+		margin: 0 auto; 
+		container-type: inline-size; 
+		overflow: auto;
+		background: yellowgreen;
+		"
+	>
+		{@render children?.()}
+	</div>
+{/snippet}
 
-	{#snippet right(toggler)}
-		<div
-			style="
-			display: flex;
-			align-items: center; 
-			justify-content: space-between;
-			padding: 0 1rem;
-			"
-		>
-			{@render toggler?.()}
-			<h2 style="flex: 1;">Lo<span style="color: aqua;">Go</span></h2>
+{#snippet content()}
+	<main style="min-height: 100vh; width: 80%; margin: 3rem auto;">
+		<h1>test</h1>
+	</main>
+{/snippet}
+
+<div style="padding: 3rem 0; background: black;">
+	{#snippet navSticky()}
+		<div style="text-align: center; margin: 5rem 0;">
+			<h2>Scroll Down</h2>
+			<p>Scroll down to see the sticky effect.</p>
 		</div>
-	{/snippet}
-</Navbar>
 
-<br />
-<br />
-
-<Navbar id="nav2" variant="collapse" align="right">
-	{#snippet left(toggler)}
-		<div
-			style="
-			display: flex;
-			align-items: center; 
-			justify-content: space-between;
-			padding: 0 1rem;
-			margin-bottom: 1rem;
-			"
-		>
-			{@render toggler?.()}
-			<h2>Lo<span style="color: aqua;">Go</span></h2>
-		</div>
-	{/snippet}
-
-	{#snippet center()}
-		<InputSearch variant="contained" placeholder="test..." />
-	{/snippet}
-
-	{#snippet right()}
-		<a href="/">
-			<Navbar.Item aria-current="page">active</Navbar.Item>
-		</a>
-		<a href="/">
-			<Navbar.Item>test</Navbar.Item>
-		</a>
-		<Menu id="two" class="border-0">
-			{#snippet anchor()}
-				<Navbar.Item>Open menu</Navbar.Item>
+		<Navbar id="nav-sticky" style="position: sticky; top: 0;">
+			{#snippet left(toggler)}
+				{@render logoWithToggler?.(toggler)}
 			{/snippet}
 
-			{#snippet items(handleClose)}
-				<Menu.Item onclick={handleClose}>test1</Menu.Item>
+			{#snippet right()}
+				<Navbar.Item variant="contained">test1</Navbar.Item>
+				<Navbar.Item variant="outlined">test2</Navbar.Item>
+			{/snippet}
+		</Navbar>
+		{@render content?.()}
+	{/snippet}
+
+	{@render container?.(navSticky)}
+
+	<br />
+	<br />
+
+	{#snippet nav1()}
+		<Navbar id="nav1" variant="full" align="left">
+			{#snippet left()}
 				<a href="/">
-					<Menu.Item>test2</Menu.Item>
-				</a>
-				<a href="/">
-					<Menu.Item>test3</Menu.Item>
+					<Navbar.Item aria-current="page">active</Navbar.Item>
 				</a>
 			{/snippet}
-		</Menu>
-		<Navbar.Item variant="contained">test4</Navbar.Item>
-		<Navbar.Item variant="outlined">test5</Navbar.Item>
-	{/snippet}
-</Navbar>
 
-<main style="min-height: 100vh; width: 80%; margin: 3rem auto;">
-	<h1>test</h1>
-</main>
+			{#snippet center()}
+				<InputSearch variant="contained" placeholder="test..." />
+			{/snippet}
+
+			{#snippet right(toggler)}
+				{@render logoWithToggler?.(toggler)}
+			{/snippet}
+		</Navbar>
+
+		{@render content?.()}
+	{/snippet}
+
+	{@render container?.(nav1)}
+
+	<br />
+	<br />
+
+	{#snippet nav2()}
+		<Navbar id="nav2" variant="collapse" align="right">
+			{#snippet left(toggler)}
+				{@render logoWithToggler?.(toggler)}
+			{/snippet}
+
+			{#snippet right()}
+				<Menu id="two" class="border-0">
+					{#snippet anchor()}
+						<Navbar.Item>Open menu</Navbar.Item>
+					{/snippet}
+
+					{#snippet items(handleClose)}
+						<Menu.Item onclick={handleClose}>test1</Menu.Item>
+						<a href="/">
+							<Menu.Item>test2</Menu.Item>
+						</a>
+						<a href="/">
+							<Menu.Item>test3</Menu.Item>
+						</a>
+					{/snippet}
+				</Menu>
+			{/snippet}
+		</Navbar>
+
+		{@render content?.()}
+	{/snippet}
+
+	{@render container?.(nav2)}
+</div>
