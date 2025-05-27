5ceec02c775cbf19ddeadbd1f6230d0ea883235e
dxdns <37380674+dxdns@users.noreply.github.com>
2025-05-27 15:51:36 -0300

    data table example

--- src/lib/components/data-table/DataTable.svelte
+++ src/lib/components/data-table/DataTable.svelte
@@ -1,50 +1,18 @@
 <script lang="ts">
-	import type { Snippet } from "svelte"
 	import type { HTMLAttributes } from "svelte/elements"
 	import classMapUtil from "../../utils/classMapUtil.js"
 	import styles from "./DataTable.module.css"
 
 	interface Props extends HTMLAttributes<HTMLDivElement> {
-		columns?: {
-			name: string
-			label: string
-			customBodyRender?: (value: any) => ReturnType<Snippet>
-		}[]
 		data: Record<string, any>[]
-		renderExpandableRow?: (row: Record<string, any>) => ReturnType<Snippet>
 	}
 
-	let {
-		class: className = "",
-		columns,
-		data,
-		renderExpandableRow,
-		...rest
-	}: Props = $props()
-
-	const currentIndex = $state(-1)
+	let { class: className = "", data, ...rest }: Props = $props()
 
 	const columnsData =
 		data.length > 0
 			? Object.keys(data[0]).map((name) => ({ name, label: name }))
 			: []
-	const columnsNames = columns || columnsData
-	const colSpan = Boolean(renderExpandableRow)
-		? columnsNames.length + 1
-		: columnsNames.length
-
-	const customBodyRender = (value: any, index: number) => {
-		const column = columns ? columns[index] : null
-		return column?.customBodyRender ? column.customBodyRender(value) : value
-	}
-
-	function handleClick(index: number) {
-		currentIndex === index ? -1 : index
-	}
-
-	function isActive(index: number) {
-		return currentIndex === index ? styles.active : ""
-	}
 </script>
 
 <table
@@ -53,39 +21,19 @@
 >
 	<thead>
 		<tr>
-			{#if renderExpandableRow}
-				<th style="width: 5px;"></th>
-			{/if}
-			{#each columnsNames as item}
+			{#each columnsData as item}
 				<th>{item.label}</th>
 			{/each}
 		</tr>
 	</thead>
 
 	<tbody>
-		{#each data as row, index}
+		{#each data as row, _}
 			<tr>
-				{#if renderExpandableRow}
-					<td>
-						<button aria-label="expand" onclick={() => handleClick(index)}
-						></button>
-					</td>
-				{/if}
-
-				{#each columnsNames as col, colIndex}
-					<td>{customBodyRender(row[col.name], colIndex)}</td>
+				{#each columnsData as col, _}
+					<td>{row[col.name]}</td>
 				{/each}
 			</tr>
-
-			{#if renderExpandableRow}
-				<tr class={`${styles.colappse} ${isActive(index)}`}>
-					<td colspan={colSpan}>
-						<div class={styles.content}>
-							{@render renderExpandableRow(row)}
-						</div>
-					</td>
-				</tr>
-			{/if}
 		{/each}
 	</tbody>
 </table>
---
+++ src/routes/(examples)/data-table/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { DataTable } from "@/lib/index.js"
+
+	const _data = Array.from(Array(10)).map((_, index) => ({
+		id: index,
+		label: `test-${index}`
+	}))
+</script>
+
+<div style="width:1000px; margin: 5rem auto;">
+	<DataTable data={_data} />
+</div>
