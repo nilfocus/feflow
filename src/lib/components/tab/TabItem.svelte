44cebe60c78a7e22a56c9dd9b51673f68423f9b6
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-03 08:53:34 -0300

    tab item color primary theme

--- src/lib/components/tab/TabItem.svelte
+++ src/lib/components/tab/TabItem.svelte
@@ -5,14 +5,16 @@
 	interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 	let { class: className, children, ...rest }: Props = $props()
+
+	const color = "var(--ff-color-primary)"
 </script>
 
 <div
 	{...rest}
 	use:activeLineAction
-	data-line-color="#ff5722"
+	data-line-color={color}
 	data-line-height="2px"
-	style="display: flex; border-bottom: 1px solid #ff5722;"
+	style="display: flex; border-bottom: 1px solid {color}; {rest.style}"
 >
 	{@render children?.()}
 </div>
