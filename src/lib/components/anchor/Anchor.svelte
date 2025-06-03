a9ef8b117091c10bda651294974f3017b093ff4e
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-03 17:53:48 -0300

    anchor component

---
+++ src/lib/components/anchor/Anchor.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from "svelte/elements"
+	import styles from "./Anchor.module.css"
+
+	interface Props extends HTMLInputAttributes {
+		ref?: HTMLInputElement
+	}
+
+	let { class: className, ref, children, ...rest }: Props = $props()
+</script>
+
+<input
+	{...rest}
+	class={styles.controller}
+	type="checkbox"
+	id={rest.id ?? "checkbox"}
+	bind:this={ref}
+	hidden
+/>
+
+<label class={styles.container} for={rest.id ?? "checkbox"}>
+	{@render children?.()}
+</label>
---
+++ src/routes/(app)/(examples)/anchor/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Anchor, Button } from "@/lib/index.js"
+</script>
+
+<br />
+<Anchor
+	id="one"
+	onchange={(e) => {
+		console.log(e.currentTarget.checked)
+	}}
+>
+	<Button>test</Button>
+</Anchor>
