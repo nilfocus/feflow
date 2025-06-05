6d8056998cd752f531396f282ba3de77f3b717b4
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-05 13:36:04 -0300

    shortcut listener component

---
+++ src/lib/components/shortcut-listener/ShortcutListener.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onShortcutAction } from "../../actions/index.js"
+	import type { HTMLAttributes } from "svelte/elements"
+
+	interface Props extends HTMLAttributes<HTMLDivElement> {
+		keys: string[]
+		callback: (event: KeyboardEvent) => void
+		preventDefault?: boolean
+	}
+
+	let {
+		class: className = "",
+		keys,
+		callback,
+		preventDefault = true,
+		children,
+		...rest
+	}: Props = $props()
+</script>
+
+<div
+	{...rest}
+	use:onShortcutAction={{
+		keys,
+		callback,
+		preventDefault
+	}}
+>
+	{@render children?.()}
+</div>
