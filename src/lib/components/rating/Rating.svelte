278123c7d70ba00f41a71ea845373420ed9d768b
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-12 17:09:50 -0300

    rating fix styles and accept custom child

--- src/lib/components/rating/Rating.svelte
+++ src/lib/components/rating/Rating.svelte
@@ -1,59 +1,78 @@
 <script lang="ts">
-	import type { HTMLInputAttributes } from "svelte/elements"
+	import type { HTMLAttributes } from "svelte/elements"
 	import { classMapUtil, mergeStyleUtil } from "../../utils/index.js"
 	import { StarIcon } from "../../icons/index.js"
 
-	interface Props extends Omit<HTMLInputAttributes, "size"> {
+	interface Props extends HTMLAttributes<HTMLDivElement> {
 		startIn?: number
-		stars?: number
+		length?: number
 		color?: string
 		size?: string | number
+		onChange?: (index: number) => void
 	}
 
 	let {
-		class: className,
 		startIn = 0,
-		stars = 5,
+		length = 5,
 		color = "yellow",
 		size = "24px",
+		onChange,
+		children,
 		...rest
 	}: Props = $props()
 
 	let index = $state(startIn)
+	let hoverIndex = $state(startIn)
+
+	function handleChange(idx: number) {
+		index = idx === index ? -1 : idx
+		onChange?.(idx)
+	}
 
 	const style = mergeStyleUtil(
-		"display: flex; align-items: center;",
+		"display: flex; align-items: flex-start;",
 		`--color: ${color}`,
 		rest.style
 	)
 </script>
 
 <div {style}>
-	{#each Array.from(Array(stars)) as _, i}
+	{#each Array.from(Array(length)) as _, i}
 		{@const idx = i + 1}
-		<input
-			{...rest}
-			type="checkbox"
-			id={rest.id ? `${rest.id}-${idx}` : `checkbox-${idx}`}
-			hidden
-			onchange={() => {
-				index = idx === index ? -1 : idx
+		<button
+			style="all: unset; cursor: pointer;"
+			onmouseenter={() => (hoverIndex = idx)}
+			onmouseleave={() => (hoverIndex = 0)}
+			onclick={() => {
+				handleChange(idx)
 			}}
-			style={undefined}
-		/>
-		<label
-			for={rest.id ? `${rest.id}-${idx}` : `checkbox-${idx}`}
-			style="cursor: pointer;"
 		>
-			<div class={classMapUtil({ ["active"]: idx <= index })}>
-				<StarIcon height={size} width={size} />
+			<div
+				class={classMapUtil("icon", { active: idx <= (hoverIndex || index) })}
+				style="--color: {color};"
+			>
+				{#if children}
+					{@render children?.()}
+				{:else}
+					<StarIcon height={size} width={size} />
+				{/if}
 			</div>
-		</label>
+		</button>
 	{/each}
 </div>
 
 <style>
-	.active {
+	.icon {
+		transition: color 0.3s ease;
+	}
+
+	.icon.active {
 		color: var(--color);
+	}
+
+	@media (hover: hover) and (pointer: fine) {
+		.icon:hover {
+			color: var(--color);
+		}
 	}
 </style>
--- src/routes/(app)/(examples)/rating/+page.svelte
+++ src/routes/(app)/(examples)/rating/+page.svelte
@@ -3,27 +3,58 @@
 </script>
 
 <h1>default</h1>
-<Rating id="one" />
+<br />
+<Rating
+	onChange={(index) => {
+		console.log(index)
+	}}
+/>
+
+<br />
+<h1>custom child</h1>
+<br />
+<Rating startIn={4} color="red">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="24px"
+		viewBox="0 -960 960 960"
+		width="24px"
+		fill="#e3e3e3"
+	>
+		<path
+			d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"
+		/>
+	</svg>
+</Rating>
+
+<br />
+<h1>vertical</h1>
+<br />
+<Rating style="flex-direction: column;" />
 
 <br />
 <h1>n stars</h1>
-<Rating id="two" color="red" stars={3} startIn={3} />
+<br />
+<Rating color="red" length={3} startIn={3} />
 
 <br />
 <h1>Start in</h1>
+<br />
 {#each Array.from(Array(5)) as _, i}
-	<Rating id={`one-${i}`} startIn={i + 1} color="blue" />
+	<Rating startIn={i + 1} color="blue" />
 {/each}
 
 <br />
 <h1>Colors</h1>
+<br />
 {#each ["aqua", "red", "green"] as color, i}
-	<Rating id={`two-${i}`} startIn={i + 1} {color} />
+	<Rating startIn={i + 1} {color} />
 {/each}
 
 <br />
 <h1>Sizes</h1>
+<br />
 {#each [13, "2rem", "48px"] as size, i}
-	<Rating id={`three-${i}`} startIn={i + 1} {size} />
+	<Rating startIn={i + 1} {size} />
 	<br />
 {/each}
