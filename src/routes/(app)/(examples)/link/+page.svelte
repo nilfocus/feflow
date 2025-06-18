4f08fe55003be0848d6eec3bb13555c61ca9efc8
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-18 10:07:47 -0300

    link styles

--- src/routes/(app)/(examples)/link/+page.svelte
+++ src/routes/(app)/(examples)/link/+page.svelte
@@ -2,7 +2,7 @@
 	import { Link } from "@/lib/index.js"
 </script>
 
-<div style="width: 500px; margin: 3rem auto; line-height: 1.5;">
+<div style="width: 500px; margin: 3rem auto; line-height: 3;">
 	<Link href="https://dxdns.dev" target="_blank">
 		<h1>normal</h1>
 	</Link>
@@ -18,4 +18,10 @@
 	<Link pathname="/" hoverUnderline="right">
 		<h1>right</h1>
 	</Link>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatibus
+		pariatur maiores cumque <Link href="https://dxdns.dev" hoverUnderline="center">obcaecati</Link>, eum
+		quod porro harum magni quisquam sunt nihil, eos modi officia molestiae.
+		Illum minima ad ducimus.
+	</p>
 </div>
