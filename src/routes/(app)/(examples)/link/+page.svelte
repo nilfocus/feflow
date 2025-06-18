2bfa278dfaef38b67a8ee8a2b65ff0771323b413
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-18 10:13:50 -0300

    link underline example

--- src/routes/(app)/(examples)/link/+page.svelte
+++ src/routes/(app)/(examples)/link/+page.svelte
@@ -6,6 +6,9 @@
 	<Link href="https://dxdns.dev" target="_blank">
 		<h1>normal</h1>
 	</Link>
+	<Link href="https://dxdns.dev" style="text-decoration: underline;">
+		underline
+	</Link>
 	<br />
 	<Link href="https://dxdns.dev" hoverUnderline="left" target="_blank">
 		<h1>left</h1>
@@ -20,8 +23,12 @@
 	</Link>
 	<p>
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatibus
-		pariatur maiores cumque <Link href="https://dxdns.dev" hoverUnderline="center">obcaecati</Link>, eum
-		quod porro harum magni quisquam sunt nihil, eos modi officia molestiae.
-		Illum minima ad ducimus.
+		pariatur maiores cumque <Link
+			href="https://dxdns.dev"
+			hoverUnderline="center"
+		>
+			obcaecati
+		</Link>, eum quod porro harum magni quisquam sunt nihil, eos modi officia
+		molestiae. Illum minima ad ducimus.
 	</p>
 </div>
