676ce3c2386b47770e69a469473b81cb9c7cc4f6
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-18 10:24:40 -0300

    link with svg

--- src/routes/(app)/(examples)/link/+page.svelte
+++ src/routes/(app)/(examples)/link/+page.svelte
@@ -6,8 +6,24 @@
 	<Link href="https://dxdns.dev" target="_blank">
 		<h1>normal</h1>
 	</Link>
+	<br />
 	<Link href="https://dxdns.dev" style="text-decoration: underline;">
 		underline
+	</Link>
+	<br />
+	<Link href="https://dxdns.dev">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			height="16px"
+			viewBox="0 -960 960 960"
+			width="16px"
+			fill="red"
+		>
+			<path
+				d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"
+			/>
+		</svg>
+		heart
 	</Link>
 	<br />
 	<Link href="https://dxdns.dev" hoverUnderline="left" target="_blank">
