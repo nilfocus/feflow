7399efe2047cdbaec7b423ecedcd54d2bde9267f
dxdns <37380674+dxdns@users.noreply.github.com>
2025-07-02 09:57:46 -0300

    templates: landing page preview

---
+++ packages/docs/src/lib/icons/index.ts
@@ -0,0 +1,21 @@
+import DarkModeIcon from "./DarkModeIcon.svelte"
+import GithubIcon from "./GithubIcon.svelte"
+import LightModeIcon from "./LightModeIcon.svelte"
+import LinkedinIcon from "./LinkedinIcon.svelte"
+import MailIcon from "./MailIcon.svelte"
+import RedditIcon from "./RedditIcon.svelte"
+import OpenInNewIcon from "./OpenInNewIcon.svelte"
+import GoogleIcon from "./GoogleIcon.svelte"
+import FacebookIcon from "./FacebookIcon.svelte"
+
+export {
+	DarkModeIcon,
+	LightModeIcon,
+	GithubIcon,
+	LinkedinIcon,
+	MailIcon,
+	RedditIcon,
+	OpenInNewIcon,
+	GoogleIcon,
+	FacebookIcon
+}
